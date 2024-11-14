783a0f94834759c9bbe1b83c87fceec00ed26ed1
jfassett1 <fassettjayden@gmail.com>
2024-11-14 22:55:39 +0000

    env file

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initialize conda
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
+conda create -n imperceptible python=3.11 -y
+conda activate test
+
+# Install PyTorch with CUDA (if required) and other packages
+conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia -y
+conda install matplotlib -y
+pip install openai-whisper==20240930
+
