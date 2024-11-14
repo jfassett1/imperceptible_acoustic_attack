7017ae6144dd1e3b5439a66a557118a8412d669a
Jayden Fassett <fassettjayden@gmail.com>
2024-11-14 18:08:42 -0500

    fix error

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
 conda create -n imperceptible python=3.11 -y
-conda activate test
+conda activate imperceptible
 
 # Install PyTorch with CUDA (if required) and other packages
 conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia -y
