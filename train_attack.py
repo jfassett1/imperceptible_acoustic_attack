d444adab8cc4ff6b0e46d83c9c9a9687eaf3c4d4
Jayden Fassett <fassettjayden@gmail.com>
2024-11-15 15:59:33 -0500

    test

--- train_attack.py
+++ train_attack.py
@@ -6,8 +6,8 @@ from pytorch_lightning import Trainer
 from src.data import AudioDataModule
 from src.attacker import MelBasedAttackerLightning
 
-data_module = AudioDataModule(dataset_name="librispeech",num_workers=0)
+data_module = AudioDataModule(dataset_name="librispeech",batch_size=128,num_workers=0)
 model = MelBasedAttackerLightning(sec=1,prepend=True)
 
-trainer = Trainer(max_epochs=5)
+trainer = Trainer(max_epochs=5,devices=[2,3,4])
 trainer.fit(model,data_module)
