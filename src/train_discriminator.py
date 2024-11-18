562abad7c1e646155b5443079a5ea3b46c95cc74
Jayden Fassett <fassettjayden@gmail.com>
2024-11-18 14:20:38 -0500

    discriminator training & saving

---
+++ src/train_discriminator.py
@@ -0,0 +1,88 @@
+import torch
+import torchaudio
+import torch.nn as nn
+from tqdm import tqdm
+from typing import Union
+from pathlib import Path
+from whisper import log_mel_spectrogram, pad_or_trim
+from .discriminator import MelDiscriminator
+from .data import AudioDataModule
+
+#TODO: Add raw audio discriminator
+
+
+
+
+def train_mel(discriminator,
+              save_dir:Union[str, Path],
+              attack_length_sec: float = 1.,
+              dataset:str = "librispeech",
+              device: str = "cuda",
+              n_epochs: int = 15,
+              batch_size: int = 128, #Only half will be real audio, other half will be random noise
+              optimizer: str = "adam",
+              num_workers = 4  
+              ):
+    
+    half_batch = batch_size // 2
+    dataloader = AudioDataModule(dataset,
+                              batch_size=half_batch,
+                              num_workers=num_workers).train_dataloader()
+    
+
+    loss_fn = nn.BCELoss()
+    if optimizer == "adam":
+        optimizer = torch.optim.Adam(discriminator.parameters())
+
+
+    attack_n = int(attack_length_sec*100)
+    pbar = tqdm(range(n_epochs),colour="red")
+    for epoch in pbar:
+
+        losses = []
+        running_loss = 0
+        for batch in dataloader:
+            raw_audio, sampling_rate, transcript = batch
+            real = log_mel_spectrogram(pad_or_trim(raw_audio))[:,:,:attack_n].to(device)
+            real = real.unsqueeze(1) #Adding channel dimension
+            noise = torch.randn((half_batch,1,80,attack_n)).to(device) # (Half_batch, 1, 80 ,LENGTH)
+
+            labels = torch.zeros(batch_size).to(device)
+            labels[half_batch:] = 1 # (0,0,0.... 1,1,1)
+
+            # print(f"Noise shape: {noise.shape} \n X Shape: {real.shape}")
+
+            shuffle_index = torch.randperm(batch_size).to(device) # Shuffle order
+
+            x = torch.cat([real,noise],dim=0) #Concatenate along batch_size dimension
+            x = x[shuffle_index]
+            labels = labels[shuffle_index] #Shuffle input & labels
+
+
+            optimizer.zero_grad()
+
+            y_pred = discriminator(x).squeeze(dim=-1)
+
+
+            loss = loss_fn(y_pred, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        avg_loss = running_loss / len(dataloader)
+        losses.append(loss.item())
+        pbar.set_description(f"Loss = {avg_loss:.4f}")
+
+        # tqdm.write(f"Loss = {avg_loss:.4f}")
+    return
+
+if __name__ == "__main__":
+    MODEL_DIR = Path(__file__).parent.parent / "discriminator"
+    D = MelDiscriminator().to("cuda")
+    train_mel(D,MODEL_DIR,n_epochs=3,device="cuda")
+
+
+    print(D(torch.randn((1,1,80,100)).to("cuda")))
+    # print(f"Saving to {MODEL_DIR}/discriminator_weights.pth")
+    # torch.save(D.state_dict(),MODEL_DIR/"discriminator_weights.pth")
