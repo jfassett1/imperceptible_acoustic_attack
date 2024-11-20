83fcec5a13c82e979b53562a9476109125ab7230
Jayden Fassett <fassettjayden@gmail.com>
2024-11-19 23:35:24 -0500

    add rawaudio attack

--- src/attacker.py
+++ src/attacker.py
@@ -1,5 +1,5 @@
 """
-Trainable Attacker class
+Trainable Attacker classes
 
 
 """
@@ -27,11 +27,13 @@ class MelBasedAttackerLightning(LightningModule):
                  epsilon: float = 0.02,
                  target_length: int = 48000,
                  batch_size: int = 64,
-                 discriminator = Optional[nn.Module]):
+                 discriminator = Optional[nn.Module],
+                 noise_type: str = "uniform" #Options are ['uniform','normal']
+):
         super(MelBasedAttackerLightning, self).__init__()
 
         # Initialize noise as a learnable parameter
-        self.noise = nn.Parameter(torch.randn(1, 80, int(sec * 100)), requires_grad=True).to(self.device)
+        self.noise = nn.Parameter(torch.rand(1, 80, int(sec * 100)), requires_grad=True).to(self.device)
         self.prepend = prepend
         self.target_length = target_length
         self.noise_len = int(sec * 100)
@@ -92,7 +94,8 @@ class MelBasedAttackerLightning(LightningModule):
     def on_before_optimizer_step(self, optimizer):
         if self.epsilon != -1:
             with torch.no_grad():
-                self.noise.clamp_(max=self.epsilon)
+                # self.noise.clamp_(min=-80,max=0)
+                self.noise.clamp_(min=-self.epsilon, max=self.epsilon)
 
 
     def training_step(self, batch, batch_idx):
@@ -145,6 +148,122 @@ class MelBasedAttackerLightning(LightningModule):
             )
             np.save(path,y)
             return 
+    
+class RawAudioAttackerLightning(LightningModule):
+    """
+    LightningModule that prepends noise for adversarial attacks, based on
+    https://arxiv.org/pdf/2405.06134
+
+    NOTE: Assumes sample rate of 16000
+    """
+    def __init__(self,
+                 model:str = "tiny.en",
+                 sec: Union[int, float] = 1,
+                 prepend: bool = True,
+                 epsilon: float = 0.02,
+                 target_length: int = 48000,
+                 batch_size: int = 64,
+                 discriminator = Optional[nn.Module],
+                 noise_type: str = "uniform" #Options are ['uniform','normal']
+):
+        super(RawAudioAttackerLightning, self).__init__()
+
+        # Initialize noise as a learnable parameter
+        self.noise = nn.Parameter(torch.rand(1, int(sec*16000)), requires_grad=True).to(self.device)
+        self.prepend = prepend
+        self.target_length = target_length
+        self.noise_len = int(sec * 16000)
+        self.model = whisper.load_model(model).to(self.device)
+        self.epsilon = epsilon
+        self.batch_size = batch_size
+
+        self.discriminator = discriminator
+
+        #Fix sparse tensors for Pytorch Lightning
+        alignment_heads_dense = self.model.get_buffer("alignment_heads").to_dense()
+        self.model.register_buffer("alignment_heads", alignment_heads_dense, persistent=False)
+        del alignment_heads_dense
+
+
+        if "en" in model:
+            self.tokenizer = whisper.tokenizer.get_tokenizer(multilingual=False,task="transcribe")
+        else:
+            self.tokenizer = whisper.tokenizer.get_tokenizer(multilingual=True)
+        self.decoder = RawDecoder(self.model,self.tokenizer,self.device)
+        #Freezing Whisper weights
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def to(self, device):
+        #Updated to function
+        super().to(device)
+
+        self.noise = self.noise.to(device)
+
+        self.decoder.device = device
+        self.decoder.update_device(device)
+        return self
+
+    def forward(self, x):
+        x = x.to(self.device)
+        BATCH_SIZE = x.shape[0]
+        # Optionally prepend the noise
+        if self.prepend:
+            #Slice noise sized chunk from x & add noise
+            x = x[:,:,:-self.noise_len]
+            noise = self.noise.repeat(BATCH_SIZE,1,1)
+            x = torch.cat([noise, x], dim=-1)
+
+
+
+        else: #Adding Noise to tensor
+            # pad_size = x.size(2) - noise.size(2)
+            noise = log_mel_spectrogram(self.noise)
+            noise = noise.repeat(BATCH_SIZE,1,1)
+
+            padding = torch.zeros_like(x)[:,:,:-noise.shape[-1]]
+
+            noise_padded = torch.cat([noise,padding],dim=-1)
+            x = noise_padded + x
+
+        return self.decoder.get_eot_prob(x)
+    
+    # Clamps to epsilon before passing to optimizer
+    def on_before_optimizer_step(self, optimizer):
+        if self.epsilon != -1:
+            with torch.no_grad():
+                self.noise.clamp_(max=0.02)
+                # self.noise.clamp_(max=self.epsilon)
+
+
+    def training_step(self, batch, batch_idx):
+        # Unpack the batch
+        x, sampling_rate, transcript = batch
+
+        x = pad_or_trim(x)
+        x = log_mel_spectrogram(x)
+        # probs = self.forward(x) #Gets probabilities
+
+        if self.discriminator is not None:
+            loss = -torch.log(self.forward(x) + 1e-9).mean() + self.discriminator(log_mel_spectrogram(self.noise))
+        else:
+            loss = -torch.log(self.forward(x) + 1e-9).mean()
+        self.log("train_loss", loss, batch_size=self.batch_size,prog_bar=True, on_step=True, on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def dump(self, path: Union[str, Path], mel: bool = False):
+            noise = self.noise.detach().cpu().numpy()
+            np.save(path,noise)
+            return 
+
+
+
+
+
 if __name__ == "__main__":
     device = "cuda"
     model = MelBasedAttackerLightning(prepend=False).to(device)
@@ -153,7 +272,6 @@ if __name__ == "__main__":
     x = torch.tensor(x).unsqueeze(0)
     x = torch.cat([x,x,x,x],dim=0)
 
-    q = model(x)
-    print(model.dump("/home/jaydenfassett/audioversarial/imperceptible/nutsack.np.npy"))
+    # print(model.dump("/home/jaydenfassett/audioversarial/imperceptible/nutsack.np.npy"))
     # x = (x,1,1)
 
