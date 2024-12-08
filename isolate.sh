10ceb5e8379d066753d52bdec383b85c35e868bf
Jayden Fassett <fassettjayden@gmail.com>
2024-12-08 02:35:32 -0500

    change to loop

--- isolate.sh
+++ isolate.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 conda activate imperceptible
 
-cd  /home/jaydenfassett/audioversarial/imperceptible
+cd /home/jaydenfassett/audioversarial/imperceptible
 
-python train_attack.py --domain raw_audio --epochs 2 --clip_val 0.02 --gpus 2,3 --no_speech --show --frequency_decay polynomial --decay_strength 1 --dataset train-other-500
-python train_attack.py --domain raw_audio --epochs 2 --clip_val 0.02 --gpus 2,3 --no_speech --show --frequency_decay polynomial --decay_strength 5 --dataset train-other-500
-python train_attack.py --domain raw_audio --epochs 2 --clip_val 0.02 --gpus 2,3 --no_speech --show --frequency_decay polynomial --decay_strength 10 --dataset train-other-500
-python train_attack.py --domain raw_audio --epochs 2 --clip_val 0.02 --gpus 2,3 --no_speech --show --frequency_decay polynomial --decay_strength 100 --dataset train-other-500
+gamma_values=(1 5 10 20)
+
+for gamma in "${gamma_values[@]}"; do
+    python train_attack.py \
+        --domain raw_audio \
+        --epochs 15 \
+        --clip_val 0.02 \
+        --gpus 2,3 \
+        --attack_length 2 \
+        --show \
+        --frequency_penalty \
+        --gamma "$gamma" \
+        --dataset train-clean-100 \
+        --num_workers 4
+
+    echo "Train with gamma $gamma complete"
+done
