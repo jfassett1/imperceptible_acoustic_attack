e9e84638e49ec6cd34992e05b51ed894d66a946f
jfassett1 <fassettjayden@gmail.com>
2025-03-06 19:11:54 -0500

    script for calculating avg threshold

---
+++ src/masking/preprocess_threshold.py
@@ -0,0 +1,39 @@
+import torchaudio
+from torch.utils.data import DataLoader
+from .mask_threshold import generate_th_batch
+from src.data import AudioDataModule
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
+import torch
+def preprocess_dataset(dataset,output, batch_size=64):
+    datamodule = AudioDataModule(dataset,batch_size,num_workers=4)
+    dataloader = datamodule.all_dataloader()
+    # (dataset, batch_size=64, shuffle=False, num_workers=2)
+    pbar = tqdm(enumerate(dataloader),total=len(dataloader))
+
+    mean_threshold = None
+    for i, (waveform, sample_rate, transcript) in pbar:
+        # print(waveform.shape)
+        waveform = waveform[:,:16000]
+        if batch_size != 1:
+            sample_rate = sample_rate[0]
+        
+        theta_xs, psd_max, PSD = generate_th_batch(audio=waveform, fs=sample_rate)
+        threshold_i = theta_xs.mean(axis=0)
+        #Online mean calculation
+        if mean_threshold is None:
+            mean_threshold = threshold_i
+        else:
+            a = 1/ (i+1)
+            b = 1 - a
+            mean_threshold = a * threshold_i + b * mean_threshold
+    np.save(output,mean_threshold)
+    print(f"Dataset Preprocessed.\nSaving final threshold to {output}")
+    return mean_threshold
+
+
+if __name__ == "__main__":
+
+    # print("s")
+    preprocess_dataset(dataset="dev-clean",output="/home/jaydenfassett/audioversarial/imperceptible/thresholds/tst")
