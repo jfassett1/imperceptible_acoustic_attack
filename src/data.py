96c7b30581580fc3f3a1339eccccc378d2ce1326
jfassett1 <fassettjayden@gmail.com>
2025-03-17 14:16:30 -0400

    add descriptive atttributes

--- src/data.py
+++ src/data.py
@@ -12,17 +12,27 @@ root_dir = Path(__file__).parent.parent
 data_dir = Path("/media/drive1/jaydenfassett/audio_data")
 # data_dir = root_dir / "data"
 
+PAD_PLACEHOLDER = 1.337e-10
 
 class AudioDataModule(pl.LightningDataModule):
-    def __init__(self, dataset_name:str ="librispeech:dev-clean", batch_size=32, num_workers=0):
+    def __init__(self, dataset_name:str ="librispeech:dev-clean", attack_len = 1,batch_size=32, num_workers=0):
         super().__init__()
         self.dataset_name, self.split = dataset_name.split(":")
         self.batch_size = batch_size
         self.num_workers = num_workers
-
+        self.iqr = None
+        self.q3 = None
+        self.q1 = None
         self.max = None
         self.min = None
+        self.attack_len = attack_len
         self.setup()
+        #Search for a sample > 4 seconds
+        while True:
+            samp_id = np.random.randint(0,len(self.dataset))
+            if self.dataset[samp_id][0].shape[-1] >= 16000 * 5:
+                self.sample = self.dataset[samp_id]
+                break
 
     def prepare_data(self):
         # Download the dataset if it is not already available TODO: Full conditionals
@@ -31,7 +41,7 @@ class AudioDataModule(pl.LightningDataModule):
                 self.dataset = torchaudio.datasets.LIBRISPEECH(data_dir, self.split, download=True)
 
             case "tedlium": 
-                self.dataset = torchaudio.datasets.TEDLIUM(data_dir, subset=self.split, audio_ext=".flac",download=True)
+                self.dataset = torchaudio.datasets.TEDLIUM(data_dir, release="release3", subset=self.split, audio_ext=".flac",download=True)
                 self.dataset._load_audio = types.MethodType(_load_audio_patched, self.dataset) #Monkey Patching original load_data function, because it does not default to the correct backend.
 
             case "commonvoice":
@@ -50,7 +60,7 @@ class AudioDataModule(pl.LightningDataModule):
     def _normalize_3std(self,audio):
         pass
     def get_IQR(self,
-                 N=5,
+                 N=10,
                  sec_audio = 5): # Length of audio
         """
         Calculates IQR of dataset by random sampling
@@ -59,19 +69,23 @@ class AudioDataModule(pl.LightningDataModule):
         
         """
         start = time()
-        dl = self.random_all_dataloader()
-        print(f"Dataset length: {len(dl)}")
+        dl = self.padded_dataloader()
+        # print(f"Dataset length: {len(dl)}")
         if (len(dl)) < N:
             N = len(dl)
         pbar = tqdm(enumerate(dl),total=N-1)
 
-        size_test = 16000 * 5 # NOTE: Only first n seconds to avoid zeroes from pad_or_clip
+        size_test = int(16000 * self.attack_len) # NOTE: 
         size_flat = self.batch_size * size_test
-        waveform_values = np.zeros(N * size_flat)
+        waveform_values = np.zeros(N * size_flat,dtype=np.float32) #TODO: FIX DTYPE
         size_gb = 0
         valid = 0
+
         print("Calculating Data Statistics...")
-        for i, (waveform, sample_rate, transcript) in pbar:
+        for i, (waveform, sample_rate, transcript) in pbar: # Waveform is (N, 480000)
+            
+            # print(type(waveform))
+
             waveform = waveform[:, :size_test]
             # print(waveform.shape)
             if waveform.size(0) < self.batch_size:  # Skip batch if's not the right size
@@ -84,27 +98,36 @@ class AudioDataModule(pl.LightningDataModule):
             start_idx = i * size_flat
             end_idx = start_idx + size_flat
             waveform_values[start_idx:end_idx] = waveform.cpu().numpy().flatten()
-
             # Update progress bar with memory usage
+
             pbar.set_description(f"Memory Usage: {size_gb:.3f} GB")
             valid += 1
             if valid == N:
                 break
+        # Removing zeroes as to not include padding
+        print(f"Removing {(waveform_values  == PAD_PLACEHOLDER).sum()} padded values from calculation")
+        waveform_values = waveform_values[waveform_values != PAD_PLACEHOLDER]
+
         # Compute global IQR
         q1, q3 = np.percentile(waveform_values, [25, 75])
         iqr = q3 - q1
         # print(iqr)
         print(f"Completed in {(time() - start):.6f} seconds")
+        self.iqr = iqr
+        self.q3 = q3
+        self.q1 = q1
         return iqr, q3, q1
 
-    def collate_fn(self, batch): #Collate not the problem
+    def collate_fn(self, batch,test=False): #Collate not the problem
         # Collate function to handle variable-length audio sequences
 
         audio, sampling_rate,transcript,utt,speak,chap= zip(*batch)
-        # print(audio[0].shape)
+        #No need to trim
+        if self.dataset_name == "librispeech":
+            resized_audio = [pad_or_trim_patch(aud[:,16000:]) if test else pad_or_trim(aud[:,16000:]) for aud in audio] #Trimming first sec for librispeech
+        else:
+            resized_audio = [pad_or_trim_patch(aud) if test else pad_or_trim(aud) for aud in audio]
 
-        #No need to trim 
-        resized_audio = [pad_or_trim(aud) for aud in audio]
         audio_signals = torch.cat(resized_audio,dim=0)
         # labels = torch.tensor(labels)
         return audio_signals, sampling_rate, transcript
@@ -121,8 +144,49 @@ class AudioDataModule(pl.LightningDataModule):
                           num_workers=self.num_workers, shuffle=False, collate_fn=self.collate_fn)        
     def random_all_dataloader(self):
         return DataLoader(self.dataset, batch_size=self.batch_size, 
-                    num_workers=self.num_workers, shuffle=False, collate_fn=self.collate_fn)  
+                    num_workers=self.num_workers, shuffle=False, collate_fn=self.collate_fn)
+    def padded_dataloader(self):
+        return DataLoader(self.dataset, batch_size=self.batch_size, 
+                    num_workers=self.num_workers, shuffle=True, collate_fn=lambda x: self.collate_fn(x,test=True))
+    def _normalize(self,data):
+        """
+        Normalizing audio based on IQR.
+        """
 
+        if None in (self.iqr,self.q3,self.q1):
+            print(self.get_IQR(5))
+        q = 1.5
+        lower,upper = (self.q1 - q*self.iqr),(self.q3 + q*self.iqr)
+        print("Upper/Lower:",upper,lower)
+        # data_x = torch.clamp(data,min=lower,max=upper)
+        return data,lower,upper
+
+
+N_SAMPLES = 480000
+import torch.nn.functional as F
+def pad_or_trim_patch(array, length: int = N_SAMPLES, *, axis: int = -1):
+    """
+    Pad or trim the audio array to N_SAMPLES, as expected by the encoder.
+    """
+    if torch.is_tensor(array):
+        if array.shape[axis] > length:
+            array = array.index_select(
+                dim=axis, index=torch.arange(length, device=array.device)
+            )
+
+        if array.shape[axis] < length:
+            pad_widths = [(0, 0)] * array.ndim
+            pad_widths[axis] = (0, length - array.shape[axis])
+            array = F.pad(array, [pad for sizes in pad_widths[::-1] for pad in sizes],value=PAD_PLACEHOLDER)
+    else:
+        if array.shape[axis] > length:
+            array = array.take(indices=range(length), axis=axis)
+
+        if array.shape[axis] < length:
+            pad_widths = [(0, 0)] * array.ndim
+            pad_widths[axis] = (0, length - array.shape[axis])
+            array = np.pad(array, pad_widths)
+    return array
 
 def _load_audio_patched (self, path: str, start_time: float, end_time: float, sample_rate: int = 16000):
 
@@ -130,41 +194,79 @@ def _load_audio_patched (self, path: str, start_time: float, end_time: float, sa
     end_time = int(float(end_time) * sample_rate)
     kwargs = {"frame_offset": start_time, "num_frames": end_time - start_time, "backend":"soundfile"}
     return torchaudio.load(path, **kwargs)
+
 if __name__ == "__main__":
-    ""
-    qr = AudioDataModule(dataset_name="tedlium:dev",batch_size=128,num_workers=0)
-    iqr, q3, q1 = qr.get_IQR(N=10)
-    print(iqr)
-    # qq = AudioDataModule(dataset_name="librispeech:train-clean-100").all_dataloader()
-    # qr = torchaudio.datasets.TEDLIUM(data_dir, subset="dev", audio_ext=".flac",download=True)
-    # qq = torchaudio.datasets.LIBRISPEECH(data_dir, "dev-clean", download=True)
+    # ""
 
-    # qr._load_audio = types.MethodType(_load_audio_patched, qr)
-    # qq[5]
-    # print(f"Lib Read Time: {(time() - start):.6f} seconds")
-    # qr[5]
-    # print(f"Ted Read Time: {(time() - start):.6f} seconds")
-    # start = time()
-    # for i in tqdm(qq,total=len(qq)):
-    #     pass
-    # print(f"Lib Read Time: {(time() - start):.6f} seconds")
-    # start = time()
-    # for i in tqdm(qr,total=len(qr)):
-    #     pass
-    # print(f"Ted Read Time: {(time() - start):.6f} seconds")
+    # dataset = torchaudio.datasets.LIBRISPEECH(data_dir, "dev-clean", download=True)
 
+    # num_zeroes = 0
+    # total_len = 0
+    # for sample in tqdm(dataset):
+    #     aud = sample[0].squeeze()
 
-
-    # waveform, sample_rate, transcript, talk_id, speaker_id, identifier = qr[1000]
-    # print(qr._path)
-
-    # p = q.get_IQR(N=5)
-    # q = AudioDataModule(dataset_name="librispeech:train-clean-100")
-    # p = q.get_IQR(N=5)
-    # print(p)
-    # dl = q.train_dataloader()
+    #     zero_mask = (aud == 0.0)
+    #     # print(aud.shape)
+    #     # exit()
+    #     num_zeroes += zero_mask.sum()
+    #     total_len += len(aud)
     
-    # for i in range(1):
-    #     l = iter(dl)
-    #     print(next(l)[0].shape)
-    # print(librispeech)
+    # print(num_zeroes)
+    # print(total_len)
+    # print(num_zeroes/total_len)
+
+    qr = AudioDataModule(dataset_name="librispeech:train-clean-100",attack_len=2,batch_size=128,num_workers=0)
+    qr.get_IQR(N=10)
+    dl = qr.padded_dataloader()
+    samp = next(iter(dl))[0]
+
+    normalized,lower,upper = qr._normalize(samp)
+    # lower,upper = lower*0.02,upper*0.02
+    # print(normalized.shape)
+    normalized = normalized.flatten()
+    print("Num of pads removed", (normalized == PAD_PLACEHOLDER).sum()/ len(normalized))
+    filtered = normalized[normalized != PAD_PLACEHOLDER]
+    print("Percentage of vals on bound",((filtered == lower) | (filtered == upper)).sum() / len(filtered ))
+
+    filtered = filtered.numpy()
+
+    # iq = qr.get_IQR(N=10)
+    # print(iq)
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from scipy.stats import norm
+    plt.figure()
+    plt.hist(filtered, bins=200, density=True, alpha=0.6, color='g')
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, np.mean( filtered), np.std( filtered))
+    plt.plot(x, p, 'k', linewidth=2)
+    plt.title("Histogram and Normal Curve")
+    plt.show()
+    plt.savefig("/home/jaydenfassett/audioversarial/imperceptible/test.png")
+    # print(filtered[:2_000_000])
+    fig, axes = plt.subplots(2, 1, figsize=(10, 6))
+
+    # First subplot: Filtered data with constraint
+
+    length = 5*16000
+    axes[0].plot(filtered[:length])
+    axes[0].set_title("Filtered with Constraint")
+    axes[0].axvline(x=16000, color="g", linestyle="--")
+    axes[0].axhline(y=lower, color='r', linestyle='--')
+    axes[0].axhline(y=upper, color='r', linestyle='--')
+    axes[0].set_ylim(-1, 1)
+    axes[0].set_xlim(0, length)
+    # Second subplot: First 16000 samples
+    axes[1].plot(filtered[:16000* qr.attack_len])
+    axes[1].set_title("First second")
+    axes[1].axvline(x=16000, color="g", linestyle="--")
+    axes[1].axhline(y=lower, color='r', linestyle='--')
+    axes[1].axhline(y=upper, color='r', linestyle='--')
+    axes[1].set_ylim(-1, 1)
+    axes[1].set_xlim(0, 16000)
+
+    # Adjust layout and save the figure
+    # plt.tight_layout()
+    plt.savefig("/home/jaydenfassett/audioversarial/imperceptible/test2.png")
+    plt.show()
