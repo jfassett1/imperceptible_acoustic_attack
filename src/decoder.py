f3b37c8810164f3e015f939a5eaeaba2598fd704
jfassett1 <fassettjayden@gmail.com>
2025-03-27 18:20:04 -0400

    fix massive decoder bug

--- src/decoder.py
+++ src/decoder.py
@@ -17,9 +17,8 @@ class RawDecoder():
     """
     Modified Whisper decoder which pulls out target given sequence
     NOTE: Doesn't support beamsearch
-
-
     """
+    
     def __init__(self,model,tokenizer,device):
         super(RawDecoder, self).__init__()
         self.model = model.to(device)
@@ -41,44 +40,45 @@ class RawDecoder():
         """
         eot_id = self.tokenizer.eot
         no_speech_id = self.tokenizer.no_speech
-        sot_tokens = torch.tensor(self.tokenizer.sot_sequence_including_notimestamps).unsqueeze(dim=0).to(self.device)
+        sot_seq = self.tokenizer.sot_sequence_including_notimestamps
+
+        sot_tokens = torch.tensor(sot_seq).unsqueeze(dim=0).to(self.device)
         logits = self.forward(mel,sot_tokens)
         probs = logits.softmax(dim=-1)
-        eot_probs = probs[:,0,eot_id]
-        no_speech_probs = probs[:,0,no_speech_id]
-        # print("PROBS",eot_probs)
-        return eot_probs,no_speech_probs,probs[:,0,:]
+
+        eot_probs = probs[:,-1,eot_id] # Pull out LAST decoded token. This is a sequence of all input tokens and their next values.
+        no_speech_probs = probs[:,-1,no_speech_id]
+        return eot_probs,no_speech_probs, probs[:,-1,:]
 
     def transcribe(self,x): # Whisper transcribe
         return self.model.transcribe(x)
 
-    def autoregressive(self,mel,n=15):
+    def autoregressive(self,mel,n=15,sot_tokens=None):
         batch_size = mel.shape[0]
-        sot_tokens = torch.tensor(self.tokenizer.sot_sequence_including_notimestamps).unsqueeze(dim=0).to(self.device)
-        tokens = torch.zeros(size=(batch_size, n + len(sot_tokens)),dtype=torch.int64).to(self.device)
+        if sot_tokens is None:
+            sot_tokens = torch.tensor(self.tokenizer.sot_sequence_including_notimestamps).unsqueeze(dim=0).to(self.device)
+        tokens = torch.zeros(size=(batch_size, n + sot_tokens.shape[-1]),dtype=torch.int64).to(self.device)
         
         tokens[:,:sot_tokens.shape[1]] = sot_tokens
         eot_id = self.tokenizer.eot
-        
 
         for i in range(n):
-            logits = self.forward(mel,tokens[:,:i+len(sot_tokens)])
+            print(i)
+            logits = self.forward(mel,tokens[:,:i+sot_tokens.shape[1]])
+            # print(logits.a)
             pred = logits[:,-1].argmax(dim=-1)
-            tokens[0:,i+len(sot_tokens)] = pred
-            if n == None:
+            tokens[0:,i+sot_tokens.shape[1]] = pred
+            print(pred)
+            if n is None:
                 if pred == eot_id:
                     break
                 
         return tokens
 
-            # likelihoods.append(softmax[eot_id])
-        # print(likelihoods)
-        # print(tokens)
-        
 if __name__ == "__main__":
     x = whisper.load_audio("/home/jaydenfassett/audioversarial/imperceptible/original_audio.wav")
     q = whisper.load_audio("/home/jaydenfassett/audioversarial/imperceptible/audio_with_attack.wav")
-    device = "cuda"
+    device = "cuda:3"
     # print("before:",x.shape)
     # x = np.concatenate([x,x,x])
     # print("after",x.shape)
@@ -93,15 +93,9 @@ if __name__ == "__main__":
     mels = torch.cat((prep(q),prep(x)),dim=0).to(device)
     print(mels.shape)
 
-    NAME = "tiny.en"
-    tokenizer = whisper.tokenizer.get_tokenizer(multilingual=False,task="transcribe")
+    NAME = "tiny"
+    tokenizer = whisper.tokenizer.get_tokenizer(multilingual=True,)
     model = whisper.load_model(NAME)
-
-    sot_tokens = torch.tensor(tokenizer.sot_sequence_including_notimestamps).unsqueeze(dim=1).to(device)
+    mel = mels[0].unsqueeze(0)
     qq = RawDecoder(model=model,tokenizer=tokenizer,device=device)
-
-    print(qq.transcribe(x)['text'])
-    # ll = qq.get_eot_prob(mels)
-    # print(ll)
-
-    
+    print(qq.get_eot_prob(mel))
